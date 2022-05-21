4a3e5966eeb753c2c1ade61094036c9d7ec8d6bd
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-21 02:31:24 +0200

    Time: 921 ms (5.96%), Space: 0B (100.00%) - LeetHub

---
+++ 584-find-customer-referee/584-find-customer-referee.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+/* Not Equal Operator: !=
+Evaluates both SQL expressions and returns 1 if they are not equal and 0 if they are equal, or NULL if either expression is NULL. If the expressions return different data types, (for instance, a number and a string), performs type conversion.*/
+
+# SELECT name FROM Customer
+# WHERE referee_id != 2
+# OR referee_id is null
+
+# SELECT name FROM Customer
+# WHERE referee_id <> 2
+# OR referee_id is null
+
+SELECT name from Customer
+WHERE id not in (
+    SELECT id FROM Customer 
+    WHERE referee_id=2);
