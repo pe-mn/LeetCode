36cf73c450ae72a8a6c14023b137171a797d9e7c
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-21 17:14:50 +0200

    Time: 633 ms (41.00%), Space: 0B (100.00%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+SELECT employee_id, 
+# IF(condition, value_if_true, value_if_false)
+IF(employee_id%2=1 and name not like 'M%',salary,0) 
+AS bonus FROM employees;
+
+
+# SELECT employee_id,
+# CASE
+#     WHEN employee_id%2=1 AND SUBSTRING(name, 1,1) <> 'M' 
+#     THEN salary
+#     ELSE 0
+# END AS bonus
+# FROM Employees;
