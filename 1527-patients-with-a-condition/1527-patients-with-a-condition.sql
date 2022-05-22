4fb5a20c1e4d2f4ad342385fb0ce2072a7a71401
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-22 16:57:51 +0200

    Time: 1020 ms (5.03%), Space: 0B (100.00%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,16 +1,10 @@
 # Write your MySQL query statement below
 
-# SELECT *
-# FROM Patients
-# WHERE conditions LIKE('%DIAB1%') 
+SELECT *
+FROM Patients
+WHERE conditions LIKE('% DIAB1%') 
+OR conditions LIKE 'DIAB1%'
 
 #------------------------------------------------------------------------------------
 
-# SELECT *
-# FROM Patients
-# WHERE conditions LIKE('% DIAB1%') 
-# OR conditions LIKE 'DIAB1%'
-
-#------------------------------------------------------------------------------------
-
-SELECT * FROM patients WHERE conditions REGEXP '\\bDIAB1'
+# SELECT * FROM patients WHERE conditions REGEXP '\\bDIAB1'
