6b01829f28fb50bb152d347ca2b32ecd10763213
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-22 14:49:00 +0200

    Time: 563 ms (83.38%), Space: 0B (100.00%) - LeetHub

---
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+# THIS SELECT STATMENT CHANGES ONLY THE VIEW
+# I TRIED USING UPDATE SET, BUT IT DID NOT WORK!!
+SELECT user_id, 
+CONCAT(UPPER(SUBSTRING(name, 1, 1)) , LOWER(SUBSTRING(name, 2))) AS name 
+FROM Users
+ORDER BY user_id;
