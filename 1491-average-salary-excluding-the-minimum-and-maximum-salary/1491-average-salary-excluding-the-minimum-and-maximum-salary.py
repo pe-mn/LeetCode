e97c8bfff031b735769ed96fe0c0b37113a42dec
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-22 23:30:05 +0200

    Time: 35 ms (77.23%), Space: 14 MB (11.12%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -0,0 +1,11 @@
+# https://www.geeksforgeeks.org/remove-all-the-occurrences-of-an-element-from-a-list-in-python/
+
+class Solution:
+    def average(self, salary: List[int]) -> float:
+        salary_new = [ i for i in salary if i not in [min(salary),max(salary)] ]
+        return statistics.mean(salary_new)
+#----------------------------------------------------------------------------------------
+
+        # salary_new = list(filter((min(salary),max(salary)).__ne__, salary))
+        # return statistics.mean(salary_new)    
+        
