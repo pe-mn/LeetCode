52aec82eba13fe0921004253dcb1b6511e514b89
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-22 16:56:49 +0200

    Time: 381 ms (34.55%), Space: 0B (100.00%) - LeetHub

---
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+# SELECT *
+# FROM Patients
+# WHERE conditions LIKE('%DIAB1%') 
+
+#------------------------------------------------------------------------------------
+
+# SELECT *
+# FROM Patients
+# WHERE conditions LIKE('% DIAB1%') 
+# OR conditions LIKE 'DIAB1%'
+
+#------------------------------------------------------------------------------------
+
+SELECT * FROM patients WHERE conditions REGEXP '\\bDIAB1'
