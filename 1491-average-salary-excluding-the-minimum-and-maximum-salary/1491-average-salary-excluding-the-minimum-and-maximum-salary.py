99b3b13b814c190b891d3015079744933e19856d
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-23 04:15:49 +0200

    Time: 76 ms (5.36%), Space: 13.8 MB (55.22%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -1,5 +1,7 @@
 # https://www.geeksforgeeks.org/remove-all-the-occurrences-of-an-element-from-a-list-in-python/
 
+# import numpy as np
+
 class Solution:
     def average(self, salary: List[int]) -> float:
         # salary_new = [ i for i in salary if i not in [min(salary),max(salary)] ]
@@ -7,7 +9,19 @@ class Solution:
     
 #----------------------------------------------------------------------------------------
 
-        salary_new = list(filter((min(salary)).__ne__, salary))
-        salary_new = list(filter((max(salary)).__ne__, salary_new))
-        return statistics.mean(salary_new)     
-        
+        # salary_new = list(filter((min(salary)).__ne__, salary))
+        # salary_new = list(filter((max(salary)).__ne__, salary_new))
+        # return statistics.mean(salary_new)     
+
+#----------------------------------------------------------------------------------------
+
+# Note: use sort after set as set don't keep order
+        salary_new = list(set(salary))
+        salary_new.sort()
+        return statistics.mean(salary_new[1:-1])
+    
+#----------------------------------------------------------------------------------------
+    
+# since array of unique integers 
+        # salary.sort()
+        # return statistics.mean(salary[1:-1])
