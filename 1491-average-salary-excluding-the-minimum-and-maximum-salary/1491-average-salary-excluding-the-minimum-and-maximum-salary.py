e06f044b3f23c34bf717210bf68d04deb743b182
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-23 04:04:05 +0200

    Time: 62 ms (10.97%), Space: 13.8 MB (96.42%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -2,10 +2,12 @@
 
 class Solution:
     def average(self, salary: List[int]) -> float:
-        salary_new = [ i for i in salary if i not in [min(salary),max(salary)] ]
-        return statistics.mean(salary_new)
+        # salary_new = [ i for i in salary if i not in [min(salary),max(salary)] ]
+        # return statistics.mean(salary_new)
+    
 #----------------------------------------------------------------------------------------
 
-        # salary_new = list(filter((min(salary),max(salary)).__ne__, salary))
-        # return statistics.mean(salary_new)    
+        salary_new = list(filter((min(salary)).__ne__, salary))
+        salary_new = list(filter((max(salary)).__ne__, salary_new))
+        return statistics.mean(salary_new)     
         
