160ae321dbf915f339b35e612575dd0139e5e0b4
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-26 00:15:59 +0200

    Time: 218 ms (65.39%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,19 +1,44 @@
-# Write your MySQL query statement below
+# Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
 
-SELECT MAX(salary) AS SecondHighestSalary 
-FROM (SELECT salary FROM Employee WHERE salary < 
-      (SELECT MAX(salary) FROM Employee)) T
- 
-# ------------------------------------------------------------------------------------
+# filter out the max
+# USE < OR <> OR NOT IN
 
-# SELECT MAX(SALARY) AS SecondHighestSalary
-# FROM EMPLOYEE
-# WHERE SALARY<(SELECT MAX(SALARY) FROM EMPLOYEE);
- 
-# ------------------------------------------------------------------------------------
+# SELECT MAX(salary) AS SecondHighestSalary FROM Employee
+# WHERE salary < (SELECT MAX(salary) FROM Employee)
 
-# SELECT DISTINCT SALARY AS SecondHighestSalary
-# FROM EMPLOYEE 
-# ORDER BY SALARY DESC
-# LIMIT 1
-# OFFSET 1;
+# SELECT MAX(salary) AS SecondHighestSalary 
+# FROM (SELECT salary FROM Employee WHERE salary < 
+#       (SELECT MAX(salary) FROM Employee)) T
+      
+#----------------------------------------------------------------------------------
+      
+# No need of sub queries,
+# just skip one row and select second rows after order by descending
+# ------------------------
+# SELECT salary AS SecondHighestSalary
+# FROM Employee
+# ORDER BY salary DESC 
+# LIMIT 1 
+# OFFSET 1
+
+# SELECT DISTINCT salary AS SecondHighestSalary
+# FROM Employee
+# ORDER BY salary DESC 
+# LIMIT 1, 1
+
+# However, this solution will be judged as 'Wrong Answer' if there is no such second highest salary since there might be only one record in this table. To overcome this issue, we can take this as a temp table.
+
+#----------------------------------------------------------------------------------
+
+SELECT
+    (SELECT DISTINCT Salary FROM Employee
+     ORDER BY Salary DESC
+     LIMIT 1, 1) AS SecondHighestSalary
+
+# SELECT
+#     IFNULL(
+#       (SELECT DISTINCT Salary
+#        FROM Employee
+#        ORDER BY Salary DESC
+#         LIMIT 1 OFFSET 1),
+#     NULL) AS SecondHighestSalary
