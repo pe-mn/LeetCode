66cf8d7f7614ebaa6c4f83ed9bf0c9fd594d1068
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-25 22:28:46 +0200

    Time: 35 ms (72.48%), Space: 13.9 MB (9.78%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -12,20 +12,20 @@ class Solution:
 # -----------------------------------------------------------------   
 
         # # l = map(int, str(n))
-        l = [int(i) for i in str(n)]
-        return math.prod(l)-sum(l)    
+        # l = [int(i) for i in str(n)]
+        # return math.prod(l)-sum(l)    
  
 # -----------------------------------------------------------------   
 
-## Best Approach
-# # if the number can be divided by 10, the the product is 0
-# # if not, then the remaining (n%10) is the first digit
-# # n//10 --> gives the remaining digits
-#         prod=1
-#         Sum=0
-#         while n>0:
-#             a=n%10
-#             prod*=a
-#             Sum+=a
-#             n=n//10
-#         return prod-Sum
+# Best Approach
+# if the number can be divided by 10, the the product is 0
+# if not, then the remaining (n%10) is the first digit
+# n//10 --> gives the remaining digits
+        prod=1
+        Sum=0
+        while n>0:
+            a=n%10
+            prod*=a
+            Sum+=a
+            n=n//10
+        return prod-Sum
