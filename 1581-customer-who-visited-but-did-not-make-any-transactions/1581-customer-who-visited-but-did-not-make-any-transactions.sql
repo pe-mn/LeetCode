f334ab175cc50b589f79f31b137cb601af955472
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-26 20:01:40 +0200

    Time: 1574 ms (20.63%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -4,6 +4,11 @@
 # WHERE visit_id NOT IN (SELECT DISTINCT visit_id FROM Transactions)
 # GROUP BY customer_id
 
-SELECT customer_id, COUNT(visit_id) AS count_no_trans FROM Visits 
-WHERE visit_id NOT IN (SELECT DISTINCT visit_id FROM Transactions)
+# SELECT customer_id, COUNT(visit_id) AS count_no_trans FROM Visits 
+# WHERE visit_id NOT IN (SELECT DISTINCT visit_id FROM Transactions)
+# GROUP BY customer_id
+
+SELECT customer_id, COUNT(visit_id) AS count_no_trans 
+FROM Visits LEFT JOIN Transactions USING(visit_id)
+WHERE transaction_id IS NULL
 GROUP BY customer_id
