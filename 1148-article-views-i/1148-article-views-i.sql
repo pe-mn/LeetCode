3ba6fbd83c83658a8787a6502c4701f8ec84a500
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-26 20:20:26 +0200

    Time: 382 ms (81.31%), Space: 0B (100.00%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
 
-# SELECT author_id AS id FROM Views
-# WHERE author_id = viewer_id
-# GROUP BY author_id
-# ORDER BY id
-
-SELECT DISTINCT author_id AS id FROM Views
+SELECT author_id AS id FROM Views
 WHERE author_id = viewer_id
+GROUP BY author_id
 ORDER BY id
+
+# SELECT DISTINCT author_id AS id FROM Views
+# WHERE author_id = viewer_id
+# ORDER BY id
