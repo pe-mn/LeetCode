a02a9726fcca8a34efcd15880935b9dfb24b286e
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-27 23:51:46 +0200

    Time: 74 ms (60.38%), Space: 14 MB (78.68%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -1,26 +1,26 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
-#         # Handle 0s
-#         if 0 in nums:
-#             return(0)
+        # Handle 0s
+        if 0 in nums:
+            return(0)
         
-#         # Handle -ve cases
-#         i = 0
-#         for num in nums:
-#             if num < 0:
-#                 i+=1
+        # Handle -ve cases
+        i = 0
+        for num in nums:
+            if num < 0:
+                i+=1
         
-#         return -1 if i%2 else 1
+        return -1 if i%2 else 1
 
 # --------------------------------------------------------------
 
 # # [Python3] line sweep   
 # -----------------------
-        ans = 1
-        for x in nums: 
-            if x == 0: return 0 
-            if x < 0: ans *= -1
-        return ans   
+        # ans = 1
+        # for x in nums: 
+        #     if x == 0: return 0 
+        #     if x < 0: ans *= -1
+        # return ans   
     
 # --------------------------------------------------------------
          
