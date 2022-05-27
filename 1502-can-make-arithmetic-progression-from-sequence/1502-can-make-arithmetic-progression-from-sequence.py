194df4d187a8343f1a1dc1c6ab391bf34be4b3ab
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-28 00:55:42 +0200

    Time: 55 ms (49.08%), Space: 14 MB (66.63%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -1,19 +1,19 @@
 class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
         
-        # Handle Edge Cases
-        if len(arr) < 2:
-            return True
+#         # Handle Edge Cases
+#         if len(arr) < 2:
+#             return True
         
-        # arr = sorted(arr)
-        arr.sort()
-        diff = arr[1] - arr [0]
+#         # arr = sorted(arr)
+#         arr.sort()
+#         diff = arr[1] - arr [0]
                         
-        for i in range(1, len(arr)-1):
-            if arr[i+1] - arr [i] != diff:
-                return False
+#         for i in range(1, len(arr)-1):
+#             if arr[i+1] - arr [i] != diff:
+#                 return False
                 
-        return True
+#         return True
 
 # --------------------------------------------------------------------
 
@@ -26,8 +26,8 @@ class Solution:
         # return len(s) == len(arr) and all(diff % gap == 0 for diff in s)                    
 # --------------------------------------------------------------------
             
-        # arr.sort()
-        # return len(set(arr[i-1] - arr[i] for i in range(1, len(arr)))) == 1        
+        arr.sort()
+        return len(set(arr[i-1] - arr[i] for i in range(1, len(arr)))) == 1        
     
 # --------------------------------------------------------------------
 
