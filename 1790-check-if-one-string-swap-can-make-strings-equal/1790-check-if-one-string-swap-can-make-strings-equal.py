91fd42400c7b23120fa1508254e5d37d820c1488
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-28 03:22:56 +0200

    Time: 43 ms (52.01%), Space: 13.7 MB (98.33%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -1,19 +1,30 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:       
-        if s1 == s2:
-            return True
+#         if s1 == s2: # i.e diff = 0
+#             return True
         
-        if sorted(s1) != sorted(s2):
-            return False
+#         if sorted(s1) != sorted(s2):
+#             return False
         
-        diff = 0
-        for i, j in zip(s1, s2):
-            if i != j:
-                diff += 1
+#         diff = 0
+#         for i, j in zip(s1, s2):
+#             if i != j:
+#                 diff += 1
                 
-        return True if diff == 2 else False
+#         return True if diff == 2 else False
 
+# -----------------------------------------------------------------------
+
+# sorted(s1)==sorted(s2) is having TC: O(NlogN). Here is a similar approach for O(N)
+
+        count = 0
+        for ch1, ch2 in zip(s1, s2):
+            if ch1 != ch2: count += 1
+        return (Counter(s1) == Counter(s2)) and (count == 0 or count == 2)
+    
 # -----------------------------------------------------------------------
         
         # diff = [[x, y] for x, y in zip(s1, s2) if x != y]
         # return not diff or len(diff) == 2 and diff[0][::-1] == diff[1]
+        
+        
