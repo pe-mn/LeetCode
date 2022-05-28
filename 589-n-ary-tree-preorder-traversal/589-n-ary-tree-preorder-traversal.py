ef0f00473b673129d8b7aa3c972c62b3c3f41467
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-28 23:27:10 +0200

    Time: 74 ms (40.78%), Space: 16.2 MB (48.55%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
@@ -61,50 +61,50 @@ class Solution(object):
         :type root: Node
         :rtype: List[int]
         """
-        if root is None:
-            return []
+#         if root is None:
+#             return []
         
-        stack = [root]
-        output = []
+#         stack = [root]
+#         output = []
         
-        # Till there is element in stack the loop runs.
-        while stack:           
-            #pop the last element from the stack and store it into temp.
-            temp = stack.pop()
+#         # Till there is element in stack the loop runs.
+#         while stack:           
+#             #pop the last element from the stack and store it into temp.
+#             temp = stack.pop()
             
-            # append. the value of temp to output
-            output.append(temp.val)
+#             # append. the value of temp to output
+#             output.append(temp.val)
             
-            #add the children of the temp into the stack in reverse order.
-            # children of 1 = [3,2,4], if not reveresed then 4 will be popped out first from the stack.
-            # if reversed then stack = [4,2,3]. Here 3 will pop out first.
-            # This continues till the stack is empty.
-            stack.extend(temp.children[::-1])
+#             #add the children of the temp into the stack in reverse order.
+#             # children of 1 = [3,2,4], if not reveresed then 4 will be popped out first from the stack.
+#             # if reversed then stack = [4,2,3]. Here 3 will pop out first.
+#             # This continues till the stack is empty.
+#             stack.extend(temp.children[::-1])
         
-        #return the output
-        return output
+#         #return the output
+#         return output
     
 # --------------------------------------------------------------------------
 
-#         """
-#         :type root: Node
-#         :rtype: List[int]
-#         """      
-#         output =[]       
-#         # perform dfs on the root and get the output stack
-#         self.dfs(root, output)        
-#         # return the output of all the nodes.
-#         return output
+        """
+        :type root: Node
+        :rtype: List[int]
+        """      
+        output =[]       
+        # perform dfs on the root and get the output stack
+        self.dfs(root, output)        
+        # return the output of all the nodes.
+        return output
     
-#     def dfs(self, root, output):       
-#         # If root is none return 
-#         if root is None:
-#             return
+    def dfs(self, root, output):       
+        # If root is none return 
+        if root is None:
+            return
         
-#         # for preorder we first add the root val
-#         output.append(root.val)
+        # for preorder we first add the root val
+        output.append(root.val)
         
-#         # Then add all the children to the output
-#         for child in root.children:
-#             self.dfs(child, output)
+        # Then add all the children to the output
+        for child in root.children:
+            self.dfs(child, output)
         
