03986cbe7ad69d759d657590ef291a61633a0216
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-28 18:48:35 +0200

    Time: 89 ms (20.43%), Space: 16 MB (97.59%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
@@ -29,25 +29,25 @@ class Solution(object):
 
 # In this problem we just need to do what is asked: preordrer traversal. Create stack, where we put root of out tree. Then on each step we extract last element from stack, add it to out and then traverse all children. Note, that we need to traverse children in the reversed order, similar to what we did for binary tree, where we traversed first right and then left.
 
-        if not root: return []
-        stack, out = [root], []
-        while stack:
-            cur = stack.pop()
-            out.append(cur.val)
-            # for child in cur.children[::-1]:
-            #     stack += [child]
-            stack.extend(reversed(cur.children))
-        return out
+        # if not root: return []
+        # stack, out = [root], []
+        # while stack:
+        #     cur = stack.pop()
+        #     out.append(cur.val)
+        #     # for child in cur.children[::-1]:
+        #     #     stack += [child]
+        #     stack.extend(reversed(cur.children))
+        # return out
     
 # I think the code will be cleaner if the last for loop is changed to stack.extend(cur.children[::-1])
 
-        # stack,s=[root],[]
-        # while stack:
-        #     p=stack.pop()
-        #     if p:
-        #         s.append(p.val)
-        #         for i in range(len(p.children)-1,-1,-1): stack.append(p.children[i])
-        # return s
+        stack,s=[root],[]
+        while stack:
+            p=stack.pop()
+            if p:
+                s.append(p.val)
+                for i in range(len(p.children)-1,-1,-1): stack.append(p.children[i])
+        return s
     
 # - Since only the left side was used, it could be replaced by stack.
 # - Besides, space complexity of "node.children[::-1]" is the max length of all node.children. It could potentially be space-consuming, when some nodes have long .children.
