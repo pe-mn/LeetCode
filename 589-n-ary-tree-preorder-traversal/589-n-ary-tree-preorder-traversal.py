5e6bbc61962f679e1fe63faa2baa31c1e2b49f11
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-28 18:47:56 +0200

    Time: 53 ms (83.54%), Space: 16 MB (97.59%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
@@ -13,12 +13,12 @@ class Solution(object):
 
 # Python short iterative solution beats 100% // 66 ms faster than fastest !
 # --------------------------------------------------------------------------
-        ret, q = [], root and [root]
-        while q:
-            node = q.pop()
-            ret.append(node.val)
-            q += [child for child in reversed(node.children) if child]
-        return ret 
+        # ret, q = [], root and [root]
+        # while q:
+        #     node = q.pop()
+        #     ret.append(node.val)
+        #     q += [child for child in reversed(node.children) if child]
+        # return ret 
         
 # Nice but reversed function is much faster than [::-1], and is more explicit:
 
@@ -29,15 +29,15 @@ class Solution(object):
 
 # In this problem we just need to do what is asked: preordrer traversal. Create stack, where we put root of out tree. Then on each step we extract last element from stack, add it to out and then traverse all children. Note, that we need to traverse children in the reversed order, similar to what we did for binary tree, where we traversed first right and then left.
 
-        # if not root: return []
-        # stack, out = [root], []
-        # while stack:
-        #     cur = stack.pop()
-        #     out.append(cur.val)
-        #     # for child in cur.children[::-1]:
-        #     #     stack += [child]
-        #     stack.extend(reversed(cur.children))
-        # return out
+        if not root: return []
+        stack, out = [root], []
+        while stack:
+            cur = stack.pop()
+            out.append(cur.val)
+            # for child in cur.children[::-1]:
+            #     stack += [child]
+            stack.extend(reversed(cur.children))
+        return out
     
 # I think the code will be cleaner if the last for loop is changed to stack.extend(cur.children[::-1])
 
