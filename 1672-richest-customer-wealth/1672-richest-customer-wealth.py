537c236c22e9d2a519d76c36c5451c36c749c718
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-29 22:07:44 +0200

    Time: 259 ms (5.08%), Space: 31.9 MB (31.15%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -15,7 +15,7 @@ class Solution:
 
 # -----------------------------------------------------------------
     
-         return max(np.sum(accounts,axis=1))
+         # return max(np.sum(accounts,axis=1))
 
 # -----------------------------------------------------------------
 
@@ -23,9 +23,9 @@ class Solution:
 # For every person, add his wealth from all the banks ie. for every row, sum all the column values and store in totalWealth.
 # If totalWealth is more than maxWealth, then update maxWealth.
 
-	    # maxWealth = 0
-	    # for i in range(len(accounts)):
-	    # totalWealth = sum(accounts[i])
-	    # maxWealth = max(maxWealth, totalWealth)
-	    # return maxWealth
+	    maxWealth = 0
+	    for i in range(len(accounts)):
+	        totalWealth = sum(accounts[i])
+	        maxWealth = max(maxWealth, totalWealth)
+	    return maxWealth
         
