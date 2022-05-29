1a8e845fba957512823e4120aa335854f6bc9160
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-29 19:18:58 +0200

    Time: 100 ms (40.09%), Space: 13.8 MB (65.17%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.py
@@ -3,9 +3,33 @@
 
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        subs = []
+    #     subs = []
+    #     for n in range(1, len(arr)+1, 2):
+    #         for i in range(len(arr)+1-n):
+    #             subs.append(arr[i:i+n])
+    #     return sum(sum(s) for s in subs)
+    
+    
+        res = 0
         for n in range(1, len(arr)+1, 2):
             for i in range(len(arr)+1-n):
-                subs.append(arr[i:i+n])
-        return sum(sum(s) for s in subs)
+                res += sum(arr[i:i+n])
+        return res
+    
+# Solution 1: Brute Force
+# ------------------------
+# Enumerate all possible odd length of subarray.
+# Time O(n^3)
+# Space O(1)
+
+        # n = len(arr)
+        # res = 0
+        # for l in range(1, n + 1, 2):
+        #     for i in range(n - l + 1):
+        #         res += sum(arr[i:i + l])
+        # return res
+    
+# Python 1 line
+# --------------
+        # return sum(sum(arr[i:i + l]) for l in range(1, 100, 2) for i in range(len(arr) - l + 1))
             
