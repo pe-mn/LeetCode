2161e6fb2ef05c555da3c6696479ca3ffc751a83
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-29 17:16:03 +0200

    Time: 107 ms (23.10%), Space: 14.4 MB (27.53%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
@@ -2,19 +2,35 @@
 # [[0,0],[0,1],[0,-1]]
 
 class Solution:   
-    def slope(self, l1, l2):
-        if l2[0] == l1[0]:
-            return 10000        
-        return (l2[1]-l1[1])/(l2[0]-l1[0]) 
+    # def slope(self, l1, l2):
+    #     if l2[0] == l1[0]:
+    #         return 10000        
+    #     return (l2[1]-l1[1])/(l2[0]-l1[0]) 
             
-    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        s = self.slope(coordinates[0], coordinates[1])        
-        if all(self.slope(coordinates[0], coordinates[i]) == s for i in range(2, len(coordinates))):
-            return True     
-        return False
+    # def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+    #     s = self.slope(coordinates[0], coordinates[1])        
+    #     if all(self.slope(coordinates[0], coordinates[i]) == s for i in range(2, len(coordinates))):
+    #         return True     
+    #     return False
     
 # https://stackoverflow.com/questions/3813681/checking-to-see-if-3-points-are-on-the-same-line
 # You can check if the area of the ABC triangle is 0:
+
+# --------------------------------------------------------------------------
+
+# In order to avoid being divided by 0, use multiplication form:
+# ---------------------------------------------------------------
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        (x0, y0), (x1, y1) = coordinates[: 2]
+        for x, y in coordinates:
+            if (x1 - x0) * (y - y1) != (x - x1) * (y1 - y0):
+                return False
+        return True
+ 
+
+    # def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+    #     (x0, y0), (x1, y1) = coordinates[: 2]
+    #     return all((x1 - x0) * (y - y1) == (x - x1) * (y1 - y0) for x, y in coordinates)
     
 
     
