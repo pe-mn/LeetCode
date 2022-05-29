e4a68fe3fa7b3db99fb0d75dd639754c064df0b6
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-29 22:05:26 +0200

    Time: 103 ms (13.31%), Space: 14 MB (31.15%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        wealth = [sum(l) for l in accounts]
+        return max(wealth)
+    
+ # -----------------------------------------------------------------
+   
+        # return max(sum(acc) for acc in accounts)
+
+# -----------------------------------------------------------------
+
+        # return max(map(sum, accounts))
+
+# -----------------------------------------------------------------
+    
+         # return max(np.sum(accounts,axis=1))
+
+# -----------------------------------------------------------------
+
+# Approach:
+# For every person, add his wealth from all the banks ie. for every row, sum all the column values and store in totalWealth.
+# If totalWealth is more than maxWealth, then update maxWealth.
+
+	    # maxWealth = 0
+	    # for i in range(len(accounts)):
+	    # totalWealth = sum(accounts[i])
+	    # maxWealth = max(maxWealth, totalWealth)
+	    # return maxWealth
+        
