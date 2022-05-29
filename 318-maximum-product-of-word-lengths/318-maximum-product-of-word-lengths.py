4ed3a113589ddb8fcf4c5ad07ad0c0677f43d0e6
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-29 05:31:22 +0200

    Time: 341 ms (91.46%), Space: 14.4 MB (42.09%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-        res = [0]
-        for i, word in enumerate(words):
-            for j in range(i+1, len(words)):
-                if all(char not in set(words[j]) for char in set(word)):
-                    res.append(len(word)*len(words[j]))
-        return max(res)
+        # res = [0]
+        # for i, word in enumerate(words):
+        #     for j in range(i+1, len(words)):
+        #         if all(char not in set(words[j]) for char in set(word)):
+        #             res.append(len(word)*len(words[j]))
+        # return max(res)
     
 # ------------------------------------------------------------------
 
@@ -13,8 +13,8 @@ class Solution:
         # for i, word in enumerate(words):
         #     for j in range(i+1, len(words)):
         #         check = True
-        #         for char in word:
-        #             if char in words[j]:
+        #         for char in set(word):
+        #             if char in set(words[j]):
         #                 check = False
         #                 break
         #         if check:
@@ -23,13 +23,13 @@ class Solution:
         
 # ------------------------------------------------------------------
 
-        # d = {}
-        # for w in words:
-        #     mask = 0
-        #     for c in set(w):
-        #         mask |= (1 << (ord(c) - 97))
-        #     d[mask] = max(d.get(mask, 0), len(w))
-        # return max([d[x] * d[y] for x in d for y in d if not x & y] or [0])
+        d = {}
+        for w in words:
+            mask = 0
+            for c in set(w):
+                mask |= (1 << (ord(c) - 97))
+            d[mask] = max(d.get(mask, 0), len(w))
+        return max([d[x] * d[y] for x in d for y in d if not x & y] or [0])
     
 # It would be better if you replace 97 with ord('a') to avoid magic numbers.
 
