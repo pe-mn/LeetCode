a9281f0f464c1d4077330d49b2717df51283f07e
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-29 05:21:20 +0200

    Time: 4041 ms (21.21%), Space: 19.1 MB (5.70%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -1,22 +1,22 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-        # res = [0]
-        # for i, word in enumerate(words):
-        #     for j in range(i+1, len(words)):
-        #         if all(char not in word for char in words[j]):
-        #             res.append(len(word)*len(words[j]))
-        # return max(res)
-    
-# ------------------------------------------------------------------
-
         res = [0]
         for i, word in enumerate(words):
             for j in range(i+1, len(words)):
-                check = True
-                for char in word:
-                    if char in words[j]:
-                        check = False
-                        break
-                if check:
+                if all(char not in words[j] for char in word):
                     res.append(len(word)*len(words[j]))
         return max(res)
+    
+# ------------------------------------------------------------------
+
+        # res = [0]
+        # for i, word in enumerate(words):
+        #     for j in range(i+1, len(words)):
+        #         check = True
+        #         for char in word:
+        #             if char in words[j]:
+        #                 check = False
+        #                 break
+        #         if check:
+        #             res.append(len(word)*len(words[j]))
+        # return max(res)
