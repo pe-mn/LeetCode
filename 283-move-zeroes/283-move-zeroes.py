538b31f93966d85a5e0c953bb5db4a92ff5398df
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-29 21:02:17 +0200

    Time: 174 ms (85.70%), Space: 15.6 MB (17.31%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -61,8 +61,8 @@ class Solution:
         # zeros = [0] * n    
         # nums = no_zero + zeros
 
-# Correct Answer        
-# ---------------                   
+# Correct Answer (Fastest)       
+# -------------------------                   
         # count=nums.count(0)
         # nums[:]=[i for i in nums if i != 0]
         # nums+=[0]*count        
@@ -78,11 +78,20 @@ class Solution:
         #     nums.append(0)    
 # ------------------------------------------------                    
 
-        zero = 0  # records the position of "0"
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[i], nums[zero] = nums[zero], nums[i]
-                zero += 1
+        # zero = 0  # records the position of "0"
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         nums[i], nums[zero] = nums[zero], nums[i]
+        #         zero += 1
 
+# ------------------------------------------------                    
         
+# We initialize two pointers i = 0, j = 0. Iterate j over range(len(nums)), and if nums[j] != 0, we swap nums[i] and nums[j], and increment i by 1. It is easy to see the loop invariant that nums[:i+1] contains all nonzero elements in nums[:j+1] with relative order preserved. Hence when j reaches len(nums)-1, nums[:i+1] contains all nonzero elements in nums with relative order preserved.
+
+# Time complexity: O(n), space complexity: O(1).
                 
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1    
