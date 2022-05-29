f78c9951acb9b5d40a8ead833a4fd6e8b782e9aa
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-30 00:55:01 +0200

    Time: 321 ms (5.03%), Space: 33 MB (5.65%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -2,14 +2,16 @@ import numpy as np
 
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-        flat_mat = [num for sublist in mat for num in sublist]
+# 		# Creat a flat matrix
+#         flat_mat = [num for sublist in mat for num in sublist]    
+#         n = len(flat_mat)
+		
+# 		# Check if reshape is invalid
+#         if n%r:
+#             return mat
         
-        n = len(flat_mat)
-        if n%r:
-            return mat
-        
-        k = n//r         
-        return [ flat_mat[(i*k) : (i*k)+k] for i in range(r)]
+#         k = n//r     # k is the number of elements per row 
+#         return [ flat_mat[(i*k) : (i*k)+k] for i in range(r)]
     
 # -------------------------------------------------------------------
 #         flat_mat = []
@@ -19,10 +21,10 @@ class Solution:
 # -------------------------------------------------------------------
 # Solution 1 - NumPy
 # -------------------
-        # try:
-        #     return np.reshape(mat, (r, c)).tolist()
-        # except:
-        #     return mat
+        try:
+            return np.reshape(mat, (r, c)).tolist()
+        except:
+            return mat
 # -------------------------------------------------------------------
 
 # -------------------------------------------------------------------
