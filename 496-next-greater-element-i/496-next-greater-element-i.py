de87824acb9e649c28445455e5a411fbce032e04
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-29 03:41:35 +0200

    Time: 79 ms (50.57%), Space: 14.3 MB (14.19%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -3,28 +3,28 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         # nums1 contains the elements to check in nums2
-        d = {}     
-        for i,num1 in enumerate(nums2):
-            for num2 in nums2[i+1:]:
-                 if num2 > num1:
-                        d[num1] = num2
-                        break
-        return [d.get(num1,-1) for num1 in nums1]
+        # d = {}     
+        # for i,num1 in enumerate(nums2):
+        #     for num2 in nums2[i+1:]:
+        #          if num2 > num1:
+        #                 d[num1] = num2
+        #                 break
+        # return [d.get(num1,-1) for num1 in nums1]
     
 # --------------------------------------------------------------
 
-#         d, st, ans = {}, [], []
+        d, st, ans = {}, [], []
         
-#         for x in nums2:
-#             while len(st) and st[-1] < x:
-#                 d[st.pop()] = x
-#             st.append(x)
+        for x in nums2:
+            while len(st) and st[-1] < x:
+                d[st.pop()] = x
+            st.append(x)
 
-#         # for x in nums1:
-#         #     ans.append(d.get(x, -1))
-#         # return ans  
+        # for x in nums1:
+        #     ans.append(d.get(x, -1))
+        # return ans  
     
-#         return [d.get(x, -1) for x in findNums]        
+        return [d.get(x, -1) for x in nums1]        
     
     
 # TypeError: 'builtin_function_or_method' object is not subscriptable res.append[1]
