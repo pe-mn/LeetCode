ff2ad861d8a036254b27bf0102264b7ab0c15fe8
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-30 05:06:47 +0200

    Time: 69 ms (14.24%), Space: 13.7 MB (99.22%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        # if sorted(words, key=lambda word: [order.index(c) for c in word]) == words:
+        #     return True                
+        # return False
+    
+# Instead of using index() which requires finding the index of a char, a better alternative consists in building a hash map to be used in the sorting, in order to retrieve the index directly.
+
+        order_dic = dict(zip(order, range(len(order))))
+        if sorted(words, key=lambda word: [order_dic[c] for c in word]) == words:
+            return True                
+        return False
