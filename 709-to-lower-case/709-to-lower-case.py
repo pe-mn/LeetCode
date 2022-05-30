ed255081ca853279934f6e1e80beac2db156f7b3
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-30 03:38:30 +0200

    Time: 47 ms (34.80%), Space: 13.8 MB (51.90%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.py
@@ -0,0 +1,22 @@
+# A suggestion: strings are immutable, so each time you add a char to a string it creates a new string.
+# I believe it is more efficient to use a list to append, and then join at the end.
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()  
+# -----------------------------------------------------------------
+#         return "".join(chr(ord(c) + 32) if 65 <= ord(c) <= 90 else c for c in s)
+# -----------------------------------------------------------------    
+        # return "".join(chr(ord(c) + 32) if "A" <= c <= "Z" else c for c in s)
+# -----------------------------------------------------------------
+
+# The ASCII codes for A-Z is 65-90 and those for a-z is that range plus 32.
+# Edit: Stop using range(65,91) to avoid creating these on every iteration.
+
+        # res = ""
+        # for char in s:
+        #     if ord(char) >= 65 and ord(char) <= 90:
+        #         res += chr(ord(char)+32)
+        #     else:
+        #         res += char   
+        # return res
