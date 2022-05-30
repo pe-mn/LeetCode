e3b35bf1c2988bc83164ad99921a532639d019eb
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-30 03:14:27 +0200

    Time: 47 ms (55.62%), Space: 13.9 MB (76.55%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,10 +1,48 @@
+# https://leetcode.com/problems/find-the-difference/discuss/1751380/JavaC%2B%2BPython-very-very-EASY-to-go-solution
+
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        l = list(t)
-        for char in list(s):
-            l.remove(char)      
-        return l[0]
+        # l = list(t)
+        # for char in list(s):
+        #     l.remove(char)      
+        # return l[0]
+    
+# ---------------------------------------------------------------                
+    
+        c = 0
+        for cs in s: c ^= ord(cs) #ord is ASCII value
+        for ct in t: c ^= ord(ct)
+        return chr(c) #chr = convert ASCII into character
             
+# ---------------------------------------------------------------   
+        # return chr(reduce(lambda x,y: x ^ y, map(ord,s+t)))
+# ---------------------------------------------------------------                
+        # dic = {}
+        # for ch in s:
+        #     dic[ch] = dic.get(ch, 0) + 1
+        # for ch in t:
+        #     if dic.get(ch, 0) == 0:
+        #         return ch
+        #     else:
+        #         dic[ch] -= 1
+# ---------------------------------------------------------------                
+    """
+    difference
+    """
+        # diff = 0
+        # for i in range(len(s)):
+        #     diff -= ord(s[i])
+        #     diff += ord(t[i])
+        # diff += ord(t[-1])
+        # return chr(diff)
+# ---------------------------------------------------------------                
+    """
+    xor
+    """
+    #     code = 0
+    #     for ch in s + t:
+    #         code ^= ord(ch)
+    #     return chr(code)
 # ---------------------------------------------------------------                
         
 # Wrong Answer: "a", "aa"
