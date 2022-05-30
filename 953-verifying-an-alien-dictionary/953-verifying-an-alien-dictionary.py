6dcf21c34164ae2e61d3046252b8fd80905b4c1b
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-30 05:27:47 +0200

    Time: 56 ms (35.26%), Space: 13.9 MB (39.06%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.py
@@ -41,30 +41,30 @@ class Solution:
 # ---------------------------------------------------------------------------
 # https://leetcode.com/problems/verifying-an-alien-dictionary/discuss/203175/Python-straightforward-solution
 
-        ind = {c: i for i, c in enumerate(order)}
-        for a, b in zip(words, words[1:]):
-            if len(a) > len(b) and a[:len(b)] == b:
-                return False
-            for s1, s2 in zip(a, b):
-                if ind[s1] < ind[s2]:
-                    break
-                elif ind[s1] > ind[s2]:
-                    return False
-        return True
+        # ind = {c: i for i, c in enumerate(order)}
+        # for a, b in zip(words, words[1:]):
+        #     if len(a) > len(b) and a[:len(b)] == b:
+        #         return False
+        #     for s1, s2 in zip(a, b):
+        #         if ind[s1] < ind[s2]:
+        #             break
+        #         elif ind[s1] > ind[s2]:
+        #             return False
+        # return True
     
 # ---------------------------------------------------------------------------
 # https://leetcode.com/problems/verifying-an-alien-dictionary/discuss/361525/Python-Solution-with-Detailed-Explaination-for-Beginner
 
-        # dic = {}
-        # new_words = []
-        # for i, ch in enumerate(order):
-        #     dic[ch] = i
-        # for w in words:
-        #     new = []
-        #     for c in w:
-        #         new.append(dic[c])
-        #     new_words.append(new)
-        # for w1, w2 in zip(new_words, new_words[1:]):
-        #     if w1 > w2:
-        #         return False
-        # return True
+        dic = {}
+        new_words = []
+        for i, ch in enumerate(order):
+            dic[ch] = i
+        for w in words:
+            new = []
+            for c in w:
+                new.append(dic[c])
+            new_words.append(new)
+        for w1, w2 in zip(new_words, new_words[1:]):
+            if w1 > w2:
+                return False
+        return True
