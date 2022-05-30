cdc3f9abab04727dd240ebda87e91234a6ecaacf
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-30 02:57:47 +0200

    Time: 55 ms (17.62%), Space: 14 MB (10.04%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,10 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        return command.replace('()','o').replace('(al)','al')
+
+# ---------------------------------------------------------------------
+# import re
+# class Solution:
+#     def interpret(self, command: str) -> str:
+#         return re.sub('\(\)', 'o', re.sub('\(al\)', 'al', command))
+        
