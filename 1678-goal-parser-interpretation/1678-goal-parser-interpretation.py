5a5ec6c151c067347f48e48e78ee890d8310f981
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-30 02:58:03 +0200

    Time: 33 ms (79.18%), Space: 13.7 MB (96.00%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,10 +1,11 @@
-class Solution:
-    def interpret(self, command: str) -> str:
-        return command.replace('()','o').replace('(al)','al')
-
-# ---------------------------------------------------------------------
-# import re
 # class Solution:
 #     def interpret(self, command: str) -> str:
-#         return re.sub('\(\)', 'o', re.sub('\(al\)', 'al', command))
+#         return command.replace('()','o').replace('(al)','al')
+
+# ---------------------------------------------------------------------
+
+import re
+class Solution:
+    def interpret(self, command: str) -> str:
+        return re.sub('\(\)', 'o', re.sub('\(al\)', 'al', command))
         
