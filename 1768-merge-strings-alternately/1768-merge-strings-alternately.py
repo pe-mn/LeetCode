b455a98363f26dd492c5f20d25d8902298bf2c55
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-30 02:10:09 +0200

    Time: 26 ms (97.69%), Space: 13.8 MB (68.68%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -15,9 +15,19 @@ class Solution:
 # --------------------------------------------------------------------------
         # return ''.join(c1 + c2 for c1, c2 in zip_longest(word1, word2, fillvalue=''))
 # --------------------------------------------------------------------------
-        return ''.join(chain(*zip_longest(word1, word2, fillvalue='')))
-# --------------------------------------------------------------------------
+        # return ''.join(chain(*zip_longest(word1, word2, fillvalue='')))
+    
+# Since strings are immutable in python, we will be allocating O(max(m, n)) extra space (not the result) for the intermediate concatenations a + b.
+# We could avoid this by using chain on the zip:
 
+# This will pass the unmodified values straight to str.join, which will result in only allocating one new string, the result. The space requirements of join will still take up the same space, so we don't win in terms of total space complexity. We just avoid a bunch of string copying.
+# --------------------------------------------------------------------------
+        res=''
+        
+        for i in range(min(len(word1),len(word2))):
+            res += word1[i] + word2[i]
+            
+        return res + word1[i+1:] + word2[i+1:]
 # --------------------------------------------------------------------------
 
             
