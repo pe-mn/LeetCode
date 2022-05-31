844200db5c64cc965ad20b112821573c0a469ecf
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-31 21:03:11 +0200

    Time: 429 ms (66.81%), Space: 27.3 MB (26.11%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -13,10 +13,12 @@ class Solution:
 #             if ''.join(comb) not in s: 
 #                 return False
 #         return True
-
 # ---------------------------------------------------------------
+        return len({s[i - k : i] for i in range(k, len(s) + 1)}) == 1 << k
+# ---------------------------------------------------------------
+# https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/discuss/660829/Python-1-line-Follow-Up
 
-        return len({s[i:i + k] for i in range(len(s) - k + 1)}) == 2 ** k
+        # return len({s[i:i + k] for i in range(len(s) - k + 1)}) == 2 ** k
 
 # The problem should be:
 # Given a binary string s and an integer k.
