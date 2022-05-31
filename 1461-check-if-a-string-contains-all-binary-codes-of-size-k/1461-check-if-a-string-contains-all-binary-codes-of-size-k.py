06c14a88fdc1d24a11b9aaf6e190ab6454a608e4
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-31 21:00:25 +0200

    Time: 474 ms (57.96%), Space: 27.3 MB (26.11%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -0,0 +1,32 @@
+import itertools
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        # def foo(s):
+        #     yield from itertools.product(*([set(s)] * k))
+        # for comb in foo(s):
+        #     if ''.join(comb) not in s: 
+        #         return False
+        # return True
+        
+        
+#         for comb in itertools.product(set(s), repeat=k):   
+#             if ''.join(comb) not in s: 
+#                 return False
+#         return True
+
+# ---------------------------------------------------------------
+
+        return len({s[i:i + k] for i in range(len(s) - k + 1)}) == 2 ** k
+
+# The problem should be:
+# Given a binary string s and an integer k.
+# Return True if all binary code of length k is a substring of s. Otherwise, return False.
+
+# That's easy warm-up.
+# Then here come the my question.
+
+# Given a binary string s and an integer k.
+# Return True if all binary code of length k is a subsequence of s. Otherwise, return False.
+
+# The difficult will be doubled,
+# so it 2 lines solution is acceptable to solve in python.
