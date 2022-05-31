c938d8d8c3bd010395c16d6eb6a1fa0b0e833ca4
Mahmoud Nagy <pe.mn99@gmail.com>
2022-05-31 22:00:01 +0200

    Time: 382 ms (83.41%), Space: 27.2 MB (26.20%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -1,12 +1,7 @@
 import itertools
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        # def foo(s):
-        #     yield from itertools.product(*([set(s)] * k))
-        # for comb in foo(s):
-        #     if ''.join(comb) not in s: 
-        #         return False
-        # return True       
+        
 # --------------------------------------------------------------- 
 # Wrong Answer 
 # Input "1", 1
@@ -16,26 +11,37 @@ class Solution:
         #     if ''.join(comb) not in s: 
         #         return False
         # return True
+# --------------------------------------------------------------- 
+# Wrong Answer 
+# Input "1", 1
+# Output true, Expected false
+# ---------------------------        
+        # def foo(s):
+        #     yield from itertools.product(*([set(s)] * k))
+        # for comb in foo(s):
+        #     if ''.join(comb) not in s: 
+        #         return False
+        # return True            
 # ---------------------------------------------------------------
 # https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/discuss/2092441/Python-oror-2-Easy-oror-One-liner-with-explanation
 
 # when k = 2, then number of combinations is 2^k = 4 i.e {'00', '11', '01', '10'}
 
-        # return len({s[i:i+k] for i in range(len(s)-k+1)}) == 2 ** k  
+        return len({s[i:i+k] for i in range(len(s)-k+1)}) == 2 ** k  
 
 # The above code requires to find all the possible combinations in input s first before comparing with required count. This can be improved a bit more with the following approach which will kill the loop once it meets the required count:
     
-        n=len(s)
-        required_count=2 ** k        
-        seen=set()
-        for i in range(n-k+1):
-            tmp=s[i:i+k]
-            if tmp not in seen:
-                seen.add(tmp)
-                required_count-=1
-                if required_count==0: # kill the loop once the condition is met
-                    return True
-        return False 
+        # n=len(s)
+        # required_count=2 ** k        
+        # seen=set()
+        # for i in range(n-k+1):
+        #     tmp=s[i:i+k]
+        #     if tmp not in seen:
+        #         seen.add(tmp)
+        #         required_count-=1
+        #         if required_count==0: # kill the loop once the condition is met
+        #             return True
+        # return False 
     
 # ---------------------------------------------------------------
         # return len({s[i - k : i] for i in range(k, len(s) + 1)}) == 1 << k
