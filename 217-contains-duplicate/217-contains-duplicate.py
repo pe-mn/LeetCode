b455265f4f865398fadd47e670b2a6bae081e8e3
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-01 16:52:14 +0200

    Time: 720 ms (20.97%), Space: 26 MB (29.99%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,9 +1,13 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums) != len(set(nums))
+        # return len(nums) != len(set(nums))
+    
+        return len(nums) > len(set(nums))
     
 # we need only to compare the lengths 
 # no need to compare the values 
+
+# However, it actually did more work than others' common code. Their process would mostly did less iterations, thus ending sooner than yours. You don't necessarily form up the whole set.
 # -----------------------------------------------------------------
         # return sorted(nums) != sorted(list(set(nums)))
 # -----------------------------------------------------------------        
