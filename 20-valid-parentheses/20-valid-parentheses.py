c570e13dfbf12950db3f0af85c02030f2692cc86
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-03 03:17:13 +0200

    Time: 43 ms (51.88%), Space: 13.8 MB (69.51%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -2,24 +2,24 @@ class Solution:
     def isValid(self, s: str) -> bool:
         
 # [Python] is this a cheating method? accepted with 40ms, easy to understand, but
-        if len(s) % 2 != 0:
-            return False
+#         if len(s) % 2 != 0:
+#             return False
             
-        while '()' in s or '{}' in s or '[]' in s:
-            s = s.replace('{}','').replace('()','').replace('[]','')
+#         while '()' in s or '{}' in s or '[]' in s:
+#             s = s.replace('{}','').replace('()','').replace('[]','')
         
-        return s == ''
+#         return s == ''
 
 # --------------------------------------------------------------------------
 
-        # d = {'(':')', '{':'}','[':']'}
-        # stack = []
-        # for i in s:
-        #     if i in d:  # 1
-        #         stack.append(i)
-        #     elif len(stack) == 0 or d[stack.pop()] != i:  # 2
-        #         return False
-        # return len(stack) == 0 # 3
+        d = {'(':')', '{':'}','[':']'}
+        stack = []
+        for i in s:
+            if i in d:  # 1
+                stack.append(i)
+            elif len(stack) == 0 or d[stack.pop()] != i:  # 2
+                return False
+        return len(stack) == 0 # 3
 	
 # 1. if it's the left bracket then we append it to the stack
 # 2. else if it's the right bracket and the stack is empty(meaning no matching left bracket), or the left bracket doesn't match
