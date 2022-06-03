4e98ad35ac2b309d38bd51d4b277eefba28bfb45
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-03 03:22:04 +0200

    Time: 44 ms (48.90%), Space: 13.8 MB (69.51%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -12,17 +12,31 @@ class Solution:
 
 # --------------------------------------------------------------------------
 
-        d = {'(':')', '{':'}','[':']'}
-        stack = []
-        for i in s:
-            if i in d:  # 1
-                stack.append(i)
-            elif len(stack) == 0 or d[stack.pop()] != i:  # 2
-                return False
-        return len(stack) == 0 # 3
+        # d = {'(':')', '{':'}','[':']'}
+        # stack = []
+        # for i in s:
+        #     if i in d:  # 1
+        #         stack.append(i)
+        #     elif len(stack) == 0 or d[stack.pop()] != i:  # 2
+        #         return False
+        # return len(stack) == 0 # 3
 	
 # 1. if it's the left bracket then we append it to the stack
 # 2. else if it's the right bracket and the stack is empty(meaning no matching left bracket), or the left bracket doesn't match
 # 3. finally check if the stack still contains unmatched left bracket
+
+# --------------------------------------------------------------------------
+
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
+            else:
+                return False
+        return stack == []
 
         
