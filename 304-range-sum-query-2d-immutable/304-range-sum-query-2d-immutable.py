805106911805d540497174a37b4ee72b72e32b0a
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-03 02:21:21 +0200

    Time: 2330 ms (39.17%), Space: 24.7 MB (79.94%) - LeetHub

---
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/range-sum-query-2d-immutable/discuss/525902/Easy-understand-python-dynamic-programming-solution-O(m*n)
+
+# https://leetcode.com/problems/range-sum-query-2d-immutable/discuss/1716543/Simple-and-elegant-prefix-sum-matrix-with-explanation-faster-than-100
+
+class NumMatrix:
+    def __init__(self, matrix: List[List[int]]):
+        if matrix is None or not matrix:
+            return
+        n, m = len(matrix), len(matrix[0])
+        self.sums = [ [0 for j in range(m+1)] for i in range(n+1) ]
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                self.sums[i][j] = matrix[i-1][j-1] + self.sums[i][j-1] + self.sums[i-1][j] - self.sums[i-1][j-1]
+    
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        row1, col1, row2, col2 = row1+1, col1+1, row2+1, col2+1
+        return self.sums[row2][col2] - self.sums[row2][col1-1] - self.sums[row1-1][col2] + self.sums[row1-1][col1-1]        
+
+# To get the sum of area D, get the Whole Area sum of area A,B,C,D first. Then area D = Whole Area - area B - area C + area A because area B and area C both include A.
+
+    
+    
+    
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
+
+
