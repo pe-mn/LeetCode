34cdc2330dfad14e3949feb747d35788ae07da97
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-03 02:40:48 +0200

    Time: 2560 ms (30.57%), Space: 24.9 MB (69.33%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -22,30 +22,63 @@
 
 # To get the sum of area D, get the Whole Area sum of area A,B,C,D first. Then area D = Whole Area - area B - area C + area A because area B and area C both include A.
 
-class NumMatrix:
-    def __init__(self, matrix: List[List[int]]):
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if i==0 and j==0:
-                    continue
-                elif i==0:
-                    matrix[i][j] += matrix[i][j-1]
-                elif j==0:
-                    matrix[i][j] += matrix[i-1][j]
-                else:
-                    matrix[i][j] += matrix[i-1][j] + matrix[i][j-1] - matrix[i-1][j-1]
-        self.matrix = matrix
+# -----------------------------------------------------------------------------------------
+
+# class NumMatrix:
+#     def __init__(self, matrix: List[List[int]]):
+#         for i in range(len(matrix)):
+#             for j in range(len(matrix[0])):
+#                 if i==0 and j==0:
+#                     continue
+#                 elif i==0:
+#                     matrix[i][j] += matrix[i][j-1]
+#                 elif j==0:
+#                     matrix[i][j] += matrix[i-1][j]
+#                 else:
+#                     matrix[i][j] += matrix[i-1][j] + matrix[i][j-1] - matrix[i-1][j-1]
+#         self.matrix = matrix
 		
-    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        a = b = c = d = 0
-        a = self.matrix[row2][col2]
-        if row1>0 and col1>0:
-            b = self.matrix[row1-1][col1-1]
-        if row1>0:
-            c = self.matrix[row1-1][col2]
-        if col1>0:
-            d = self.matrix[row2][col1-1]
-        return (a+b) - (c+d)
+#     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+#         a = b = c = d = 0
+#         a = self.matrix[row2][col2]
+#         if row1>0 and col1>0:
+#             b = self.matrix[row1-1][col1-1]
+#         if row1>0:
+#             c = self.matrix[row1-1][col2]
+#         if col1>0:
+#             d = self.matrix[row2][col1-1]
+#         return (a+b) - (c+d)
+
+# -----------------------------------------------------------------------------------------
+
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):        
+        if not matrix:
+            # Reject empty matrix
+            return
+        
+        # get height and width of matrix (also known as image)
+        h, w = len(matrix), len(matrix[0])
+        
+        # build integral image by recurrence relationship    
+        self.integral_image = [ [ 0 for _ in range(w) ] for _ in range(h) ]
+
+        for y in range(h):
+            summation = 0
+            for x in range(w):
+                summation += matrix[y][x]
+                self.integral_image[y][x] = summation
+
+                if y > 0:
+                    self.integral_image[y][x] += self.integral_image[y-1][x]        
+        
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:        
+        bottom_right    = self.integral_image[row2][col2]
+        bottom_left     = self.integral_image[row2][col1-1] if col1 >= 1 else 0
+        top_right       = self.integral_image[row1-1][col2] if row1 >= 1 else 0
+        top_left        = self.integral_image[row1-1][col1-1] if col1 >= 1 and row1 >=1 else 0      
+        return bottom_right - bottom_left - top_right + top_left
     
     
 # Your NumMatrix object will be instantiated and called as such:
