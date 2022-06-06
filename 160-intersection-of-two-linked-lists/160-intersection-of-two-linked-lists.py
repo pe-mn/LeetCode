a63f99f1f0bcc4f6b4ad6fe57734eaecc99ec0c5
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-06 03:16:37 +0200

    Time: 261 ms (23.65%), Space: 29.6 MB (47.83%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -6,21 +6,29 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        if headA is None or headB is None:
-            return None
+#         if headA is None or headB is None:
+#             return None
 
-        pa = headA # 2 pointers
-        pb = headB
-        while pa is not pb:
-            # if either pointer hits the end, switch head and continue the second traversal, 
-            # if not hit the end, just move on to next
-            pa = headB if pa is None else pa.next
-            pb = headA if pb is None else pb.next
-
-        return pa # only 2 ways to get out of the loop, they meet or the both hit the end=None
+#         pa = headA # 2 pointers
+#         pb = headB
+#         while pa is not pb:
+#             # if either pointer hits the end, switch head and continue the second traversal, 
+#             # if not hit the end, just move on to next
+#             pa = headB if pa is None else pa.next
+#             pb = headA if pb is None else pb.next
+#         return pa # only 2 ways to get out of the loop, they meet or the both hit the end=None
 
 # the idea is if you switch head, the possible difference between length would be countered. 
 # On the second traversal, they either hit or miss. 
 # if they meet, pa or pb would be the node we are looking for, 
 # if they didn't meet, they will hit the end at the same iteration, pa == pb == None, return either one of them is the same,None
         
+# ---------------------------------------------------------------------------------------------------
+
+        a, b = headA, headB
+        while (a != b):
+            a = headB if not a else a.next
+            b = headA if not b else b.next
+        return a
+    
+# ---------------------------------------------------------------------------------------------------
