703e972984fa398ee4a741cbbd272140b0efab70
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-07 12:55:05 +0200

    Time: 90 ms (5.42%), Space: 13.8 MB (85.45%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -0,0 +1,34 @@
+# nums1 has a length of m + n
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # while m > 0 and n > 0:
+        #     if nums1[m-1] >= nums2[n-1]:
+        #         nums1[m+n-1] = nums1[m-1]
+        #         m -= 1
+        #     else:
+        #         nums1[m+n-1] = nums2[n-1]
+        #         n -= 1
+        # if n > 0:
+        #     nums1[:n] = nums2[:n]
+            
+# -----------------------------------------------------
+        while n:
+            if m and nums1[m-1] >= nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+# -----------------------------------------------------
+                
+        # nums1[m:] = nums2[:n]
+        
+#         nums1[m:] = nums2 
+#         nums1.sort()
+        
+        
+        
