1a033ecdfb8097feba06bbd54a8b22410e81e4c9
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-08 04:05:21 +0200

    Time: 797 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -6,6 +6,12 @@
 # having count(*) > 1
 
 
-SELECT DISTINCT(P1.email) FROM Person P1, Person P2
-WHERE P1.email = P2.email
-AND P1.id <> P2.id
+# SELECT DISTINCT(P1.email) FROM Person P1, Person P2
+# WHERE P1.email = P2.email
+# AND P1.id <> P2.id
+
+
+SELECT distinct p1.Email FROM Person p1
+INNER JOIN Person p2
+ON p1.Email = p2.Email
+WHERE p1.Id <> p2.Id;
