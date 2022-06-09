8f08890ab9735f35e301e4de6552c093b28a0d2d
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-09 13:57:27 +0200

    Time: 296 ms (6.86%), Space: 14.9 MB (41.52%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -22,7 +22,22 @@ class Solution:
         #     if target-num in dic:
         #         return [dic[target-num]+1, i+1]
         #     dic[num] = i
-            
+
+# binary search     
+# -------------
+        # for i in range(len(numbers)):
+        #     l, r = i+1, len(numbers)-1
+        #     tmp = target - numbers[i]
+        #     while l <= r:
+        #         mid = l + (r-l)//2
+        #         if numbers[mid] == tmp:
+        #             return [i+1, mid+1]
+        #         elif numbers[mid] < tmp:
+        #             l = mid+1
+        #         else:
+        #             r = mid-1
+
+# ----------------------------------------------------------------------   
             
 # two-pointer (Faster)
 # --------------------
@@ -36,24 +51,24 @@ class Solution:
         #         l += 1
         #     else:
         #         r -= 1
+                
+
+        l, r = 0, len(numbers)-1
+        while l < r:
+            if numbers[l] + numbers[r] == target: return (l + 1,  r + 1)
+            if numbers[l] + numbers[r] > target: r -= 1
+            else: l += 1
+                
+# Explanation:
+# The array is sorted in increasing order.
+# So, incresing left index gives bigger number and decresing right index gives smaller number.
+# We start with left index as the 1st index and right index as the last index of the array.
+# Calculate the sum of the two elements at the two indices.
+# If it is greater than the target, that means we have to decrese the sum. So, we decrement the right index.
+# If it is lesser than the target, that means we have to increse the sum. So, we inrement the left index.
+# Continue this process untill the sum is equal to the target.
     
         
-# binary search     
-# -------------
-        for i in range(len(numbers)):
-            l, r = i+1, len(numbers)-1
-            tmp = target - numbers[i]
-            while l <= r:
-                mid = l + (r-l)//2
-                if numbers[mid] == tmp:
-                    return [i+1, mid+1]
-                elif numbers[mid] < tmp:
-                    l = mid+1
-                else:
-                    r = mid-1
-
-# ----------------------------------------------------------------------        
-        # for i, num in enumerate(numbers):
-        #     if num < target:
+     
                 
         
