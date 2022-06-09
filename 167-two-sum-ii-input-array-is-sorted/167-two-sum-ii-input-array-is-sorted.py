48b44ba1921dd6f2f78fca91a6992b4ab2ed3fd2
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-09 13:38:21 +0200

    Time: 131 ms (92.45%), Space: 14.8 MB (89.05%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -17,24 +17,25 @@ class Solution:
         
 # dictionary
 # -------------
-        dic = {}
-        for i, num in enumerate(numbers):
-            if target-num in dic:
-                return [dic[target-num]+1, i+1]
-            dic[num] = i
+        # dic = {}
+        # for i, num in enumerate(numbers):
+        #     if target-num in dic:
+        #         return [dic[target-num]+1, i+1]
+        #     dic[num] = i
             
             
 # two-pointer
 # -------------
-        # l, r = 0, len(numbers)-1
-        # while l < r:
-        #     s = numbers[l] + numbers[r]
-        #     if s == target:
-        #         return [l+1, r+1]
-        #     elif s < target:
-        #         l += 1
-        #     else:
-        #         r -= 1
+# l --> left, r --> right
+        l, r = 0, len(numbers)-1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l+1, r+1]
+            elif s < target:
+                l += 1
+            else:
+                r -= 1
     
         
 # binary search     
