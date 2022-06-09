bb6545a4eab34874ec25250a6fbb7e9e5c62fe5b
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-09 13:40:37 +0200

    Time: 366 ms (5.01%), Space: 15 MB (41.52%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -24,33 +24,33 @@ class Solution:
         #     dic[num] = i
             
             
-# two-pointer
-# -------------
+# two-pointer (Faster)
+# --------------------
 # l --> left, r --> right
-        l, r = 0, len(numbers)-1
-        while l < r:
-            s = numbers[l] + numbers[r]
-            if s == target:
-                return [l+1, r+1]
-            elif s < target:
-                l += 1
-            else:
-                r -= 1
+        # l, r = 0, len(numbers)-1
+        # while l < r:
+        #     s = numbers[l] + numbers[r]
+        #     if s == target:
+        #         return [l+1, r+1]
+        #     elif s < target:
+        #         l += 1
+        #     else:
+        #         r -= 1
     
         
 # binary search     
 # -------------
-        # for i in xrange(len(numbers)):
-        #     l, r = i+1, len(numbers)-1
-        #     tmp = target - numbers[i]
-        #     while l <= r:
-        #         mid = l + (r-l)//2
-        #         if numbers[mid] == tmp:
-        #             return [i+1, mid+1]
-        #         elif numbers[mid] < tmp:
-        #             l = mid+1
-        #         else:
-        #             r = mid-1
+        for i in range(len(numbers)):
+            l, r = i+1, len(numbers)-1
+            tmp = target - numbers[i]
+            while l <= r:
+                mid = l + (r-l)//2
+                if numbers[mid] == tmp:
+                    return [i+1, mid+1]
+                elif numbers[mid] < tmp:
+                    l = mid+1
+                else:
+                    r = mid-1
 
 # ----------------------------------------------------------------------        
         # for i, num in enumerate(numbers):
