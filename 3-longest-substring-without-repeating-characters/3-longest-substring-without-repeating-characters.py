222398962afdfa248de94fffbd4747f650eb39a2
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-10 18:47:13 +0200

    Time: 66 ms (85.44%), Space: 14 MB (93.04%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -93,7 +93,14 @@ class Solution:
         #         sub_dict[s[strt+1:i-1]] = len(s[strt+1:i-1])             
         # return max(sub_dict.values())
         
-# ---------------------------------------------------------------------------    
+# ---------------------------------------------------------------------------   
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/discuss/347818/Python3%3A-sliding-window-O(N)-with-explanation     
+# Sliding window
+# ---------------
+# We use a dictionary to store the character as the key, the last appear index has been seen so far as value.
+# seen[charactor] = index
+# move the pointer when you met a repeated character in your window.
+
         dic, res, start, = {}, 0, 0
         for i, ch in enumerate(s):
             # when char already in dictionary
@@ -105,13 +112,18 @@ class Solution:
             # add/update char to/of dictionary 
             dic[ch] = i
         # answer is either in the begining/middle OR some mid to the end of string
-        return max(res, len(s)-start)  
+        return max(res, len(s)-start) 
+        # as the last substring will not enter the loop to update res
+
+        
 # ---------------------------------------------------------------------------    
 # {'p': 0}
 # {'p': 0, 'w': 1}
+# enter loop
 # {'p': 0, 'w': 2}
 # {'p': 0, 'w': 2, 'k': 3}
 # {'p': 0, 'w': 2, 'k': 3, 'e': 4}
+# enter loop
 # {'p': 0, 'w': 5, 'k': 3, 'e': 4}   
 
         # d, res, start = {}, 0, 0
@@ -119,7 +131,30 @@ class Solution:
         #     if ch in d: start = max(start, d[ch]+1)
         #     res = max(res, i-start+1)                
         #     d[ch] = i
-        # return res    
+        # return res 
+# ---------------------------------------------------------------------------    
+
+# Wrong Answer 
+# Input "abba"
+# Output 3
+# Expected 2
+# -------------
+        # dic, res, start, = {}, 0, 0
+        # for i, ch in enumerate(s):
+        #     # when char already in dictionary
+        #     if ch in dic:
+        #         # check length from start of string to index
+        #         res = max(res, i-start)
+        #         # update start of string index to the next index
+        #         start = dic[ch]+1
+        #     # add/update char to/of dictionary 
+        #     dic[ch] = i
+        # # answer is either in the begining/middle OR some mid to the end of string
+        # return max(res, len(s)-start) 
+        # # as the last substring will not enter the loop to update res
+        
+# Basically start can't move left. It can only move right. So either move it right or leave it unchanged. You only move it right when start is less than the current repetition. If start has somehow moved ahead then there's no need to move it left. For example in abba. When the second a is encountered, start has moved ahead from first a. But the map still remembers that as the last seen a. Now just to eleimitate the left a if we move start by just 1 then it will land on first b. start moved backwards. Before it was on second b and now it is on first b. Just don't update it in this case. Just update the repeated character position in the map and now back to normal, start has become less than the next repeating a, so now start can move, if it sees another a.
+   
 
 # https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
 # max(sub_dict, key=sub_dict.get)
