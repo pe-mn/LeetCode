5cc3422f4e963d59046b3639f605c231da64915a
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-11 14:29:20 +0200

    Time: 1548 ms (57.79%), Space: 28 MB (62.03%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,17 +1,36 @@
+# https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/discuss/1016199/Python-O(n)-solution-using-cumulative-sums
+
+# We can reformulate this problem: we need to choose several values from the beginning and several values from end, such that sum of this numbers is equal to x. It is equivalent to finding some contiguous subarray, such that it has sum of elements equal to sum(nums) - x, which has the biggest length. In this way problem becomes quite classical and I prefer to solve it using cumulative sums.
+
 class Solution:
-    def minOperations(self, nums: List[int], x: int) -> int:   
-        cumsum = [0] + list(accumulate(nums))
-        dic = {c:i for i,c in enumerate(cumsum)}
-        goal = cumsum[-1] - x
-        ans = -float("inf")
+    def minOperations(self, nums: List[int], x: int) -> int: 
+#         # append [0] to the beginning to modify cumsums indices
+#         cumsum = [0] + list(accumulate(nums))
+#         dic = {c:i for i,c in enumerate(cumsum)}
+#         goal = cumsum[-1] - x
+#         ans = -float("inf")
 
-        if goal < 0: return -1
+#         # This means tha x > sum(nums)
+#         if goal < 0: return -1
 
-        for num in dic:
-            if num + goal in dic:
-                ans = max(ans, dic[num + goal] - dic[num])
+#         for num in dic:
+#             if num + goal in dic:
+#                 ans = max(ans, dic[num + goal] - dic[num])
 
-        return len(nums) - ans if ans != -float("inf") else -1              
+#         return len(nums) - ans if ans != -float("inf") else -1     
+    
+# ------------------------------------------------------------------------------
+# Using sliding window to find the longest subarry that sums to sum(nums) - x
+# ------------------------------------------------------------------------------
+        target, size, win_sum, lo, n = sum(nums) - x, -1, 0, -1, len(nums)
+        for hi, num in enumerate(nums):
+            win_sum += num
+            while lo + 1< n and win_sum > target:
+                lo += 1
+                win_sum -= nums[lo]
+            if win_sum == target:
+                size = max(size, hi - lo)
+        return -1 if size < 0 else n - size
         
 # ---------------------------------        
 # Wrong Answer (Corrected)
