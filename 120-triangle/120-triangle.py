c33a2c776dbb3cea3cf9e22853b53900451c61b1
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-13 14:18:45 +0200

    Time: 137 ms (11.34%), Space: 14.9 MB (92.55%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -16,14 +16,23 @@ class Solution:
         # return triangle[0][0]
         
 # Solution - III (Bottom-Up Dynamic Programming w/ Auxillary Space)
-	    n = len(triangle)
-	    cur_row, prev_row = [0]*n, [0]*n
-	    prev_row[0] = triangle[0][0]  
-	    for level in range(1, n):
-		    for i in range(level+1):
-			    cur_row[i] = triangle[level][i] + min(prev_row[min(i, level-1)], prev_row[max(i-1,0)])
-		    cur_row, prev_row = prev_row, cur_row
-	    return min(prev_row)
+	    # n = len(triangle)
+	    # cur_row, prev_row = [0]*n, [0]*n
+	    # prev_row[0] = triangle[0][0]  
+	    # for level in range(1, n):
+	    # for i in range(level+1):
+	    # cur_row[i] = triangle[level][i] + min(prev_row[min(i, level-1)], prev_row[max(i-1,0)])
+	    # cur_row, prev_row = prev_row, cur_row
+	    # return min(prev_row)
+
+# Solution - IV (Top-Down Dynamic Programming w/ Auxillary Space)
+        n = len(triangle)
+        cur_row, next_row = [0]*n, triangle[n-1]        
+        for level in range(n-2,-1,-1):
+            for i in range(level+1):
+                cur_row[i] = triangle[level][i] + min(next_row[i], next_row[i+1])
+            cur_row, next_row = next_row, cur_row
+        return next_row[0]
               
         
 # Wrong Answer        
