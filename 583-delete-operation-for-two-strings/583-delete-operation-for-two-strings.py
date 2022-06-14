df5da66b0379426b9c07876e066a12c4c2015534
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-14 18:37:52 +0200

    Time: 310 ms (74.32%), Space: 13.9 MB (96.71%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -13,12 +13,21 @@ class Solution:
         # return m + n - 2 * dp[m][n]  
 
 # For DP in python, a convenient way to save considering the first number of char and index of string maybe as following
-        m,n=len(word1), len(word2)
-        dp=[[0]*(n+1) for _ in range(m+1)]
+#         m,n=len(word1), len(word2)
+#         dp=[[0]*(n+1) for _ in range(m+1)]
         
-        for i, w1 in enumerate(word1,1):
-            for j, w2 in enumerate(word2,1):
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]+(w1==w2))
+#         for i, w1 in enumerate(word1,1):
+#             for j, w2 in enumerate(word2,1):
+#                 dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]+(w1==w2))
         
-        return m + n - 2*dp[-1][-1] 
+#         return m + n - 2*dp[-1][-1] 
 
+# Using 1D space instead of 2D space for dp:
+        m, n = len(word1), len(word2)
+        dp = [0]*(n+1)
+        for _,w1 in enumerate(word1,1):
+            new = [0]*(n+1)
+            for j,w2 in enumerate(word2,1):
+                new[j] = max(new[j-1],dp[j],dp[j-1]+(w1==w2))
+            dp = new
+        return m+n-2*dp[n]
