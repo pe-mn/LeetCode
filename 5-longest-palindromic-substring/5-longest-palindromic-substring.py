a4c29d3b46fad403bad1ea208ed588cf50ba6cfd
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-16 15:37:41 +0200

    Time: 1487 ms (47.54%), Space: 14 MB (29.29%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -39,37 +39,37 @@ class Solution:
     
 # Similar version but matrix in reverse, so somewhat easier to understand than OPs code.
 
-        dp = [[False]*len(s) for _ in range(len(s)) ]
-        for i in range(len(s)):
-            dp[i][i]=True
+#         dp = [[False]*len(s) for _ in range(len(s)) ]
+#         for i in range(len(s)):
+#             dp[i][i]=True
             
-        ans=s[0]
-        for j in range(len(s)):
-            for i in range(j):
-                if s[i]==s[j] and (dp[i+1][j-1] or j==i+1):
-                    dp[i][j]=True
-                    if j-i+1>len(ans):
-                        ans=s[i:j+1]
-        return ans
+#         ans=s[0]
+#         for j in range(len(s)):
+#             for i in range(j):
+#                 if s[i]==s[j] and (dp[i+1][j-1] or j==i+1):
+#                     dp[i][j]=True
+#                     if j-i+1>len(ans):
+#                         ans=s[i:j+1]
+#         return ans
 
 # --------------------------------------------------------------------------------------
 # Python easy to understand solution with comments (from middle to two ends).
 
-#         res = ""
-#         for i in range(len(s)):
-#             # odd case, like "aba"
-#             tmp = self.helper(s, i, i)
-#             if len(tmp) > len(res):
-#                 res = tmp
-#             # even case, like "abba"
-#             tmp = self.helper(s, i, i+1)
-#             if len(tmp) > len(res):
-#                 res = tmp
-#         return res
+        res = ""
+        for i in range(len(s)):
+            # odd case, like "aba"
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            # even case, like "abba"
+            tmp = self.helper(s, i, i+1)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
     
-# # get the longest palindrome, l, r are the middle indexes   
-# # from inner to outer
-#     def helper(self, s, l, r):
-#         while l >= 0 and r < len(s) and s[l] == s[r]:
-#             l -= 1; r += 1
-#         return s[l+1:r]
+# get the longest palindrome, l, r are the middle indexes   
+# from inner to outer
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1:r]
