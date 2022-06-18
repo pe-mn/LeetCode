cdcfe74f6c1897b20d688874fef0f114bdd463bc
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-18 18:21:46 +0200

    Time: 1981 ms (48.93%), Space: 52.6 MB (45.57%) - LeetHub

---
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.py
@@ -0,0 +1,14 @@
+class WordFilter:
+    def __init__(self, words: List[str]):        
+        self.d = {}
+        for i, word in enumerate(words):
+            for p, s in product(range(len(word) + 1), repeat=2):
+                self.d[word[:p], word[s:]] = i
+
+    def f(self, prefix: str, suffix: str) -> int:
+        return self.d.get((prefix, suffix), -1)        
+
+
+# Your WordFilter object will be instantiated and called as such:
+# obj = WordFilter(words)
+# param_1 = obj.f(prefix,suffix)
