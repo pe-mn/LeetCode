ca34dad2e13bdf1abcb7623f44e9753a9190e9ab
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-23 13:52:26 +0200

    Time: 1517 ms (9.39%), Space: 20 MB (68.42%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.py
+++ 630-course-schedule-iii/630-course-schedule-iii.py
@@ -2,19 +2,19 @@
 
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
-        # heap, time = [], 0
-        # for t, end in sorted(courses, key=lambda x: x[1]):
-        #     time += t
-        #     heapq.heappush(heap, -t)
-        #     if time > end:
-        #         time += heapq.heappop(heap)
-        # return len(heap)
+        heap, time = [], 0
+        for t, end in sorted(courses, key=lambda x: x[1]):
+            time += t
+            heapq.heappush(heap, -t)
+            if time > end:
+                time += heapq.heappop(heap)
+        return len(heap)
 # -------------------------------------------------------------------
-        course = []
-        for i in sorted(courses, key = lambda x:x[1]):
-            course.append(i[0])
-            while sum(course) > i[1]:
-                course.remove(max(course))
-        return len(course)
+        # course = []
+        # for i in sorted(courses, key = lambda x:x[1]):
+        #     course.append(i[0])
+        #     while sum(course) > i[1]:
+        #         course.remove(max(course))
+        # return len(course)
         
         
