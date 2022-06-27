1dfab4dcf0cdf8e7117b8f1ed49460b2fd3bdf6a
Mahmoud Nagy <pe.mn99@gmail.com>
2022-06-27 14:40:52 +0200

    Time: 814 ms (12.38%), Space: 26.8 MB (97.27%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -1,9 +1,15 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        size = len(cardPoints) - k
-        minSubArraySum = curr = sum(cardPoints[:size])
+        
+# we can reverse the problem and 
+# get the minimum sum of a continuous sub array 
+        size = len(cardPoints) - k   # The size of the subArray 
+        # The sum from the 1st element to the size of the subArray 
+        minSubArraySum = curr = sum(cardPoints[:size]) 
         
         for i in range(len(cardPoints) - size):
+            # Subtract the an element from the beginning of the subArray 
+            # And Add another element at the end
             curr += cardPoints[size + i] - cardPoints[i]
             minSubArraySum = min(minSubArraySum, curr)
             
